1251f1665b45a9003e20d27a5ef389de798ec316
Lièvre Loïc <llievre@protonmail.com>
2023-11-28 19:42:55 +0100

    Transform example crypto

--- examples/test_crypto/index.js
+++ examples/test_crypto/index.js
@@ -7,24 +7,24 @@ const printHash = (hash) => {
 var a = 0;
 
 // compute a hash with salt set to "salt0"
-let hash = crypto.pbkdf2Sync("seg-2023", "salt0", 1000, 64, "sha512");
+var hash = crypto.pbkdf2Sync("seg-2023", "salt0", 1000, 64, "sha512");
 printHash(hash);
-
 
 a += 1;
 // compute a hash with salt set to "salt1"
-let hash_2 = crypto.pbkdf2Sync("seg-2023", `salt${a}`, 1000, 64, "sha512");
+var hash_2 = crypto.pbkdf2Sync("seg-2023", `salt${a}`, 1000, 64, "sha512");
 printHash(hash_2);
 
 a += 1;
 // compute a hash with salt set to "salt2"
-let hash_3 = crypto.pbkdf2Sync("seg-2023", `salt${a}`, 1000, 64, "sha512");
+var hash_3 = crypto.pbkdf2Sync("seg-2023", `salt${a}`, 1000, 64, "sha512");
 printHash(hash_3);
 
-// print 100 hash
+// print 5 hash
 // call a second time with salt0 and other one with salt"i"
 for (var i=0; i < 5; i++) {
   // create a hash of text
-  let hash = crypto.pbkdf2Sync("seg-2023", "salt"+i+a, 1000, 64, "sha512");
+  hash = crypto.pbkdf2Sync("seg-2023", "salt"+a, 1000, 64, "sha512");
   printHash(hash);
+  a+=1
 }
